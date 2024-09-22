bc63b5c755049502b9dffc36e91235d8e49f33ee
MrPen <lan474444@gmail.com>
2024-09-22 23:50:43 +0700

    added unicode :D

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
         message: "Server hosted successfully",
         subject: "Web application development",
         students: [
-            {id:"22120181", name:"Nguyen Duy Lam"},
-            {id:"22120183", name:"Nguyen Dang Minh Lan"},
-            {id:"22120186", name:"Huynh Tan Loc"},
+            {id:"22120181", name:"Nguyễn Duy Lâm"},
+            {id:"22120183", name:"Nguyễn Đặng Minh Lân"},
+            {id:"22120186", name:"Huỳnh Tấn Lộc"},
         ]
     });
 })
