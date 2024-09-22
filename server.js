888e6424483822117b97d197448a4b3ae24af090
MrPen <lan474444@gmail.com>
2024-09-22 23:47:12 +0700

    added more details

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
 
-    res.json({message: "Server hosted successfully, with 22120183"});
+    res.json({
+        message: "Server hosted successfully",
+        subject: "Web application development",
+        students: [
+            {id:"22120181", name:"Nguyen Duy Lam"},
+            {id:"22120183", name:"Nguyen Dang Minh Lan"},
+            {id:"22120186", name:"Huynh Tan Loc"},
+        ]
+    });
 })
 
 app.listen(port, ()=>{
